811d7f459afb563398bd9bb17587465880f11fca
Michael Victor Zink <zuwiki@zuwiki.net>
2011-10-15 15:36:56 -0500

    Adding exit button within the uiwebview. Pretty nifty little hack if you ask me.

--- Receptor Roulette/MZNHDocumentationLayer.h
+++ Receptor Roulette/MZNHDocumentationLayer.h
@@ -8,7 +8,7 @@
 
 #import "cocos2d.h"
 
-@interface MZNHDocumentationLayer : CCLayer {
+@interface MZNHDocumentationLayer : CCLayer <UIWebViewDelegate> {
 	UIWebView * webView;
 }
 
--- Receptor Roulette/MZNHDocumentationLayer.m
+++ Receptor Roulette/MZNHDocumentationLayer.m
@@ -20,6 +20,7 @@
 		webFrame.size = [[CCDirector sharedDirector] winSize];
 		webFrame.origin = CGPointZero;
 		self.webView = [[UIWebView alloc] initWithFrame: webFrame];
+		self.webView.delegate = self;
 		self.webView.backgroundColor = [UIColor whiteColor];
 		[self.webView setOpaque: NO];
 		NSURL * url = [[NSBundle mainBundle]
@@ -32,8 +33,14 @@
     return self;
 }
 
+// cocos2d stuff
+
 - (void) onEnterTransitionDidFinish {
 	[[[CCDirector sharedDirector] openGLView] addSubview: self.webView];
+}
+
+- (void) onExit {
+	[self.webView removeFromSuperview];
 }
 
 +(CCScene *) scene
@@ -42,6 +49,17 @@
 	MZNHDocumentationLayer *layer = [MZNHDocumentationLayer node];
 	[scene addChild: layer];
 	return scene;
+}
+
+// UIWebViewDelegate stuff
+
+- (BOOL) webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+	if ([request.URL.path isEqualToString: @"/exit!"]) {
+		[[CCDirector sharedDirector] performSelector: @selector(popScene) withObject: nil afterDelay: 0.0];
+		return NO;
+	} else {
+		return YES;
+	}
 }
 
 - (void) dealloc {
